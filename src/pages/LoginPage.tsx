6cf4d5b2bfd259a75a3be756516c1975dfce2e2f
varinder-008 <varetsingh88@gmail.com>
2024-06-12 12:41:27 +0530

    Login and Register Page built

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,11 +2,10 @@ import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-
-const LoginPage = () => {
-  return (
-    <div className='flex justify-center items-center h-screen'>
-      <Card className="w-full max-w-sm">
+import { Link } from 'react-router-dom'
+const LoginPage = () => (
+  <div className='flex justify-center items-center h-screen'>
+    <Card className="w-full max-w-sm">
       <CardHeader>
         <CardTitle className="text-2xl">Login</CardTitle>
         <CardDescription>
@@ -24,11 +23,18 @@ const LoginPage = () => {
         </div>
       </CardContent>
       <CardFooter>
-        <Button className="w-full">Sign in</Button>
+        <div className='w-full'>
+          <Button className="w-full">Sign in</Button>
+          <div className="mt-4 text-center text-sm">
+            Don't have an account?{" "}
+            <Link to={'/register'} className="underline">
+              Sign Up
+            </Link>
+          </div>
+        </div>
       </CardFooter>
     </Card>
-    </div>
-  )
-}
+  </div>
+)
 
 export default LoginPage
